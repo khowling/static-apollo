a420ed6ac93dd17d112a7e0d3c0de8a060766000
keith <khowling@gmail.com>
2021-04-08 12:47:33 +0100

    ok

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+## How it was built
+
+#  follow to create apollo function ```./apollo```
+
+https://www.apollographql.com/docs/apollo-server/deployment/azure-functions/
+
+
+
+# follow to create React assets
+
+https://github.com/facebook/create-react-app
---
+++ apollo/graphql/index.js
@@ -0,0 +1,24 @@
+const { ApolloServer, gql } = require('apollo-server-azure-functions');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// POST 
+//  query  {
+//   hello 
+// }
+
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!',
+    },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+exports.graphqlHandler = server.createHandler();
